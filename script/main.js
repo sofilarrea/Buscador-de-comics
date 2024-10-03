2c92f21fe326604a10762b41f30597763a53a3b4
sofilarrea <sofialejandra.larrea@gmail.com>
2024-10-03 20:03:39 -0300

    marvel api

--- script/main.js
+++ script/main.js
@@ -1,12 +1,18 @@
-const urlApi = 'http//developer.marvel.com';
+const urlApi = 'https://gateway.marvel.com';
 const urlPersonajes = '/v1/public/characters';
 const apiKey = '65db027dcb3242c298c3482a2b3f65d4';
+const ts = '1';
+const hash = '32e7770ba72dfc622c150d2fc1e02ac7';
 
-fetch( urlApi + urlPersonajes,{ 
+
+fetch( urlApi + urlPersonajes + `?ts=${ts}&apikey=${apiKey}&hash=${hash}`, { 
     method: 'GET', 
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + apiKey
+       /*  'Authorization': `${apiKey}`, */
         }
     }    
 )
+.then((response) => response.json())
+.then((data) => console.log(data))
+.catch((error) => console.log(error))
